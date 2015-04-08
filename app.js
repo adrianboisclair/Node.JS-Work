01af7f2708127ccab420a80a15e212b88494b122
Adrian Boisclair <adrianboisclair@gmail.com>
2015-04-07 22:34:53 -0700

    install ejs

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 // configure app
+
+app.set('view_engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 // use middleware
 // define routes
 
-app.get('/', function(req, res){
+app.get('/', function (req, res){
     res.render('index');
 });
 
 app.listen(1337, function() {
     console.log('ready on port: 1337')
 });
-
-
-
 
 
 
