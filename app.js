5b6c7ba74f767876733bc59393adb622e9ba6d61
Adrian Boisclair <adrianboisclair@gmail.com>
2015-04-07 21:58:13 -0700

    Node.JS App
    Trying to figure this stuff out.

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+
+
+
+//var http = require('http');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end('<h1>Hello Dude!</h1>\n');
+//}).listen(1337, '127.0.0.1');
+//console.log('Server fuckin\' runnin\' at http://127.0.0.1:1337/');
