d5d57ddeba61415620c4554cb775d29ae4c4f5e0
Adrian Boisclair <adrianboisclair@gmail.com>
2015-04-07 22:26:34 -0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var express = require('express');
 
 var app = express();
 
+// configure app
+// use middleware
+// define routes
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(1337, function() {
+    console.log('ready on port: 1337')
+});
+
+
+
 
 
 
