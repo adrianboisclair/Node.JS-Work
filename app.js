0d1d2a9558aa5b2509060a3affe8e7b89ccb837d
Adrian Boisclair <adrianboisclair@gmail.com>
2015-04-07 23:51:54 -0700

    messing around with node

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 
@@ -7,12 +9,42 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 // use middleware
+
+app.use(bodyParser());
+
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
 // define routes
+var todoItems = [
+    { id: 1, name: 'Adrian',desc: 'person'},
+    { id: 2, name: 'Sabrina',desc: 'person'},
+    { id: 3, name: 'Mike',desc: 'person'},
+    { id: 4, name: 'Todd',desc: 'person'},
+    { id: 5, name: 'Jeremy',desc: 'person'},
+];
 
 app.get('/', function (req, res){
-    res.render('index');
+    //load data from db
+    res.render('index', {
+        title: 'My App',
+        items: todoItems
+    });
 });
+
+app.post('/add', function (req, res) {
+    var newItem = req.body.newItem;
+
+    todoItems.push({
+        id: todoItems.length + 1,
+        desc: newItem
+    });
+
+    res.redirect('/');
+    console.log(newItem);
+});
+
 
 app.listen(1337, function() {
     console.log('ready on port: 1337')
